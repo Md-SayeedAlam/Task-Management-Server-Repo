4f4fc2d1eb4b5728e2ede234f2fb85bbe1a7f3d1
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2025-02-22 12:34:20 +0600

    added vercel json file and deploy it to vecel and added may apis

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
 
@@ -144,8 +144,8 @@ app.delete('/tasks/:id',async(req,res)=>{
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
